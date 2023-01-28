91b418165e242b875b2c75ec594ed1d1fdfbabff
Desmond <desezo@gmail.com>
2023-01-28 18:57:13 +0100

    fixed filter ux issue

--- src/app/views/home-view/filter-tab/filter-tab.component.ts
+++ src/app/views/home-view/filter-tab/filter-tab.component.ts
@@ -7,15 +7,41 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class FilterTabComponent {
   btn: string = 'kõik';
+  buttonList: string [] = 
+  [
+    'Harjumaa',
+    'Lääne-Virumaa',
+    'Tartumaa',
+    'Pärnumaa',
+    'Ida-Virumaa',
+    'Viljandimaa',
+    'Raplamaa',
+    'Võrumaa',
+    'Saaremaa',
+    'Jõgevamaa',
+    'Valgamaa',
+    'Läänemaa',
+    'Hiiumaa',
+    'Järvamaa',
+  ];
 
   @Output() filterValue = new EventEmitter();
-  filterByCounty(value: string){
-    
-    if(value == ""){
-      this.btn = 'kõik';
-    }else{
-      this.btn = value;
+
+
+  moveToFirst(index: any) {
+    const clickedButton = this.buttonList.splice(index, 1);
+    this.buttonList.unshift(clickedButton[0]);
+  }
+
+  filterByCounty(value: string, i:number|null = null){
+    this.btn = value;
+    if(value == 'kõik'){
+      value = '';
     }
+    if (i) {
+      this.moveToFirst(i);
+    }
+   
     this.filterValue.emit(value);
   }
 }
