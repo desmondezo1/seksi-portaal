d4a7aca25e682d67f85636ee69326c290982433d
Desmond <desezo@gmail.com>
2023-01-31 01:01:01 +0100

    footer in progress

--- src/app/core/footer/footer.module.ts
+++ src/app/core/footer/footer.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { AppImageModule } from 'src/app/shared/components/atoms/image/image.module';
 
 
 
@@ -9,7 +10,8 @@ import { FooterComponent } from './footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppImageModule
   ],
   exports: [FooterComponent]
 
